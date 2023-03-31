33ca534eadf80e81d88925d0b509479aab06d4ec
AlenGeorg3 <alentgeorge123@gmail.com>
2023-03-31 22:18:33 +0530

    Added project files

---
+++ js/script.js
@@ -0,0 +1,25 @@
+
+// swiper implementation in JS
+import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.esm.browser.min.js'
+
+const swiper = new Swiper('.swiper', {
+    loop: true,
+
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+});
+
+// swiper implentation ends
